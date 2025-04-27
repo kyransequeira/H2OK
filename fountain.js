7dace7c8b49b0dfe6308ba394144ec9125202916
Kyra <72363204+kyransequeira@users.noreply.github.com>
2025-04-27 10:27:33 -0700

    Update fountain.js

--- fountain.js
+++ fountain.js
@@ -11,3 +11,29 @@ const fountainSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Fountain', fountainSchema);
+// backend/routes/fountains.js
+const express = require('express');
+const Fountain = require('../models/fountain');
+const router = express.Router();
+
+// Get all fountains
+router.get('/', async (req, res) => {
+  try {
+    const fountains = await Fountain.find();
+    res.json(fountains);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Update fountain status
+router.put('/:id', async (req, res) => {
+  try {
+    const fountain = await Fountain.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(fountain);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+module.exports = router;
